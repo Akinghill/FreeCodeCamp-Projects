0eb9e968d11d92532b208c518d1bf11e0aa13b26
Austin Hill <austinkinghill@gmail.com>
2020-06-23 06:35:58 -0700

    fix for fcc and some regactors

--- 05.01-Timestamp-Microservice/server.js
+++ 05.01-Timestamp-Microservice/server.js
@@ -4,6 +4,7 @@ const app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
 const cors = require('cors');
+app.use(cors({ optionSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -13,29 +14,27 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/timestamp', (req, res) => {
+  const date = new Date();
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString(),
+  });
 });
 
-app.get('/api/timestamp/:date?', function (req, res) {
-  let date = null;
+app.get('/api/timestamp/:date_str?', function (req, res) {
+  const { date_str } = req.params;
 
-  // parse the date string
-  if (req.params.date !== undefined) {
-    // check if it is a unix timestamp...
-    let unixTimestamp = parseInt(req.params.date * 1);
-    if (isNaN(unixTimestamp)) {
-      // it's not a unix timestamp string
-      date = new Date(req.params.date);
-    } else {
-      // it is a timestamp
-      date = new Date(unixTimestamp);
-    }
+  let date = new Date(date_str);
+
+  // check if it is a unix timestamp...
+  let unixTimestamp = parseInt(date_str * 1);
+  if (isNaN(unixTimestamp)) {
+    // it's not a unix timestamp string
+    date = new Date(date_str);
   } else {
-    // the date string parameter is empty.
-    // create a new date based on current time
-    date = new Date(Date.now());
+    // it is a timestamp
+    date = new Date(unixTimestamp);
   }
 
   // Initialize the resonse object, if Date is invalid
@@ -52,10 +51,8 @@ app.get('/api/timestamp/:date?', function (req, res) {
 });
 
 // 404 Not Found Middleware
-app.use(function(req, res, next) {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
+app.use(function (req, res, next) {
+  res.status(404).type('text').send('Not Found');
 });
 
 // listen for requests :)
