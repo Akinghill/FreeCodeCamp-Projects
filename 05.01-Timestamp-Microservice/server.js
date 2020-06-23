4d7f53bb2f7587e224be77e9ed36a9896736dc7b
Austin Hill <austinkinghill@gmail.com>
2020-06-22 19:25:17 -0700

    fixed package, server

--- 05.01-Timestamp-Microservice/server.js
+++ 05.01-Timestamp-Microservice/server.js
@@ -1,32 +1,29 @@
-// server.js
-// where your node app starts
-
-// init project
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
-var cors = require('cors');
-app.use(cors({ optionSuccessStatus: 200 })); // some legacy browsers choke on 204
+const cors = require('cors');
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // your first API endpoint...
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
+
 app.get('/api/timestamp/:date?', function (req, res) {
   let date = null;
+
   // parse the date string
   if (req.params.date !== undefined) {
-    // check if it is a unix timestand...
+    // check if it is a unix timestamp...
     let unixTimestamp = parseInt(req.params.date * 1);
     if (isNaN(unixTimestamp)) {
       // it's not a unix timestamp string
@@ -54,7 +51,14 @@ app.get('/api/timestamp/:date?', function (req, res) {
   res.json(response);
 });
 
+// 404 Not Found Middleware
+app.use(function(req, res, next) {
+  res.status(404)
+    .type('text')
+    .send('Not Found');
+});
+
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port 3000');
 });
